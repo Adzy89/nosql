ef5d9ccd75293795709c7ad7aa6da73ba2943cb9
adzzy89 <adam_pilato@hotmail.com>
2023-04-01 16:13:09 +1100

    changed localhost to 127.0.0.1 in index to fix server issues

---
+++ config/connections.js
@@ -0,0 +1,17 @@
+// const { connect, connections } = require('mongoose');
+
+// const connectionString =
+//   process.env.MONGODB_URI || 'mongodb://localhost:27017/nosql';
+
+// connect(connectionString, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((err) => {
+//     console.error('Failed to connect to MongoDB', err);
+//   });
+
+// module.exports = connections;
